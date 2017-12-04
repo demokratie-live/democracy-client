3f890c8d54980e4d373c1f063ee4359e27d60df1
Manuel Ruck <gitHub@jungsfinest.de>
2017-12-04 17:31:37 +0100

    add react for web

---
+++ src/App.js
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+console.log(Platform.OS);
+
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu",
+  web: "Live Realoading is enabled"
+});
+
+const welcomePlatform = () => {
+  switch (Platform.OS) {
+    case "web":
+      return `Welcome to React for Web!`;
+      break;
+    default:
+      return `Welcome to React Native!`;
+      break;
+  }
+};
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{welcomePlatform()}</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
