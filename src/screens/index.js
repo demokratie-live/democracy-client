6d54563f4d89a21f8730087464f57bba36dab732
Manuel Ruck <github@manuelruck.de>
2017-12-08 17:29:21 +0100

    add nativation to detailscreen for native

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,8 +1,13 @@
 import { Navigation } from 'react-native-navigation';
 
 import BundestagTabScreen from './List';
+import BundestagDetailsTabScreen from './ListDetails/ListDetails';
 
 // register all screens of the app (including internal ones)
 export default function registerScreens() {
   Navigation.registerComponent('democracy.BundestagTabScreen', () => BundestagTabScreen);
+  Navigation.registerComponent(
+    'democracy.BundestagDetailsTabScreen',
+    () => BundestagDetailsTabScreen,
+  );
 }
