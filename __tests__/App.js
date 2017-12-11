6405008418ae9a30da74f6ce91e6c35893861939
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2017-12-11 22:25:49 +0100

    fixed tests

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,9 +1,8 @@
 import 'react-native';
 import React from 'react';
-import App from '../src/App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import App from '../src/App';
 
 it('renders correctly', () => {
   const tree = renderer.create(
