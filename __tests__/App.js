25ba5a598e13f2c903d3fb60a48717da420d2b67
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2017-12-12 00:42:06 +0100

    fixed coverage

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,13 +1,11 @@
-// import 'react-native';
-// import React from 'react';
+import 'react-native';
+import React from 'react';
 // Note: test renderer must be required after react-native.
-// import renderer from 'react-test-renderer';
-// import App from '../src/App';
-//
-// it('renders correctly', () => {
-//   const tree = renderer.create(
-//     <App />
-//   );
-// });
-
-it('dummy', () => {});
+import renderer from 'react-test-renderer';
+import Row from '../src/components/List/Row';
+it('render row', () => {
+  const onpress = function(){};
+  const votes = 123;
+  const tree = renderer.create(<Row title="test" subtitle="test2" date="test3" votes={votes} onPress={onpress}/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
