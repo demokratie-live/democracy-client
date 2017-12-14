7fb7d6b82e988dd6850f460ab0181b07c0f5c5d1
Manuel Ruck <github@manuelruck.de>
2017-12-14 09:20:36 +0100

    add missing import com.horcrux.svg.SvgPackage;

--- android/app/src/main/java/com/democracyclient/MainApplication.java
+++ android/app/src/main/java/com/democracyclient/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import com.reactnativenavigation.NavigationApplication;
+import com.horcrux.svg.SvgPackage;
 
 import java.util.Arrays;
 import java.util.List;
