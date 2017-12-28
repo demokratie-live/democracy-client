fb840e19d74ebdb039ad2fb212fbd40a066d72cd
Manuel Ruck <github@manuelruck.de>
2017-12-28 20:14:38 +0100

    add github page

--- scripts/start.web.js
+++ scripts/start.web.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'development';
 const path = require('path');
 
 const webpackConfigProd = require('react-scripts/config/webpack.config.dev');
