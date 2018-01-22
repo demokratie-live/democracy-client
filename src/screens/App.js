1841886b49dca898095f22f7684b5db34a4a7180
Manuel Ruck <github@manuelruck.de>
2018-01-22 19:31:46 +0100

    try flow

--- src/screens/App.js
+++ src/screens/App.js
@@ -7,7 +7,7 @@
 import React from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
-const instructions = Platform.select({
+const instructions: string = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`
 });
