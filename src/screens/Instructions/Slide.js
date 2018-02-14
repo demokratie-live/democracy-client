816e5d226bfc77b4bb32b5d72db2cf002f8f5f41
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-02-14 17:08:57 +0100

    fixed flow errors

--- src/screens/Instructions/Slide.js
+++ src/screens/Instructions/Slide.js
@@ -1,12 +1,10 @@
-// @flow
-
 import React from "react";
 import { Text, View } from "react-native";
 import PropTypes from "prop-types";
 
-const Slide = props => (
-  <View style={props.style}>
-    <Text style={props.styleText}>{props.text}</Text>
+const Slide = ({ style, styleText, text }) => (
+  <View style={style}>
+    <Text style={styleText}>{text}</Text>
   </View>
 );
 
