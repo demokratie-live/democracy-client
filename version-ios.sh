ad574c529b2ac6196316b36bfb6b476d0e108c33
Manuel Ruck <github@manuelruck.de>
2018-02-23 12:59:00 +0100

    fix pathes improve android versioning

--- version-ios.sh
+++ version-ios.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash -e
 
-PROJECT_DIR="../ios/democracyclient"
+APP_NAME=$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+
+PROJECT_DIR="ios/${APP_NAME}"
 INFOPLIST_FILE="Info.plist"
 INFOPLIST_DIR="${PROJECT_DIR}/${INFOPLIST_FILE}"
 
-PACKAGE_VERSION=$(cat ../package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 
 BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_DIR}")
 BUILD_NUMBER=$(($BUILD_NUMBER + 1))
