c25be1789d6a5cff223f69574caa680d3410a9d8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-03-05 15:47:24 +0100

    dummy implementation of first tut page

--- src/screens/Instructions/Slide.js
+++ src/screens/Instructions/Slide.js
@@ -1,30 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components/native";
+import { View, Image } from 'react-native';
 
 const Container = styled.View`
   flex: 1;
-  justify-content: center;
   align-items: center;
-  background: ${({ background }) => background};
+  background: #fff;
 `;
 
 const Text = styled.Text`
-  color: #fff;
+  color: #000;
   font-size: 30;
   font-weight: bold;
 `;
 
-const Slide = ({ background, text }) => (
-  <Container background={background}>
-    <Text>{text}</Text>
-  </Container>
+const Slide = () => (
+  <Container>
+    <View style={{ height: 85, justifyContent: 'center' }}>
+      <Image source={require("../../../assets/images/logo3.png")} />
+    </View>
+    <View style={{ alignItems: 'center' }}>
+      <Text style={{ color: '#000', fontSize: 22, fontFamily: 'HelveticaNeue-Light', letterSpacing: -0.1, paddingTop: 15 }}>Wilkommen in der Beta</Text>
+      <Text style={{ color: '#9b9b9b', fontSize: 15, fontFamily: 'HelveticaNeue-Light', letterSpacing: -0.4, paddingTop: 1 }}>Alles über die deutsche Politik in einer App</Text>
+    </View>
+    <View style={{ justifyContent: 'center', marginTop: 28 }}>
+      <Image source={require("../../../assets/images/detail1.png")} style={{ position: 'absolute', left: -93, top: 29 }} />
+      <Image source={require("../../../assets/images/list.png")} style={{ position: 'absolute', left: -130, top: 0 }} />
+      <Image source={require("../../../assets/images/oval4.png")} style={{ position: 'absolute', left: 93, top: 167 }} />
+    </View>
+  </Container >
 );
-
-Slide.propTypes = {
-  background: PropTypes.string,
-  text: PropTypes.string.isRequired
-};
 
 Slide.defaultProps = {
   background: ""
--- src/screens/Instructions/index.js
+++ src/screens/Instructions/index.js
@@ -6,6 +6,7 @@ import { graphql } from "react-apollo";
 import PropTypes from "prop-types";
 import styled from "styled-components/native";
 import { Navigator } from "react-native-navigation";
+import { TouchableOpacity, Text } from 'react-native';
 
 import Slide from "./Slide";
 
@@ -13,9 +14,16 @@ import SET_INSTRUCTIONS_SHOWN from "../../graphql/mutations/setInstructinosShown
 
 const Container = styled.View`
   flex: 1;
+  backgroundColor: #fff;
 `;
 
-const Button = styled.Button``;
+const Button = styled.Button.attrs({
+  color: "#fff",
+}) `
+color: #0076ff;
+height: 60pt;
+font-size: 20;
+`;
 
 const BUTTON_TEXTS = {
   next: "Weiter >",
@@ -61,17 +69,19 @@ class Introductions extends Component {
           }}
           loop={false}
           onMomentumScrollEnd={this.onMomentumScrollEnd}
+          paginationStyle={{ marginBottom: -15 }}
+          style={{ marginBottom: 29 }}
+          dotStyle={{ width: 5, height: 5, backgroundColor: '#4494d3', opacity: 0.5, marginTop: 12 }}
+          activeDotStyle={{ width: 5, height: 5, backgroundColor: '#4494d3', marginTop: 12 }}
         >
-          <Slide background="#9DD6EB" text="1" />
-          <Slide background="#97CAE5" text="2" />
-          <Slide background="#92BBD9" text="3" />
+          <Slide background="#fff" text="1" />
+          <Slide background="#fff" text="2" />
+          <Slide background="#fff" text="3" />
         </Swiper>
-        <Button
-          onPress={this.onClick}
-          title={this.state.buttonText}
-          accessibilityLabel={this.state.buttonText}
-        />
-      </Container>
+        <TouchableOpacity onPress={this.onClick} style={{ backgroundColor: '#fff', height: 60, justifyContent: 'center' }} >
+          <Text style={{ textAlign: 'center', color: '#0076ff', fontSize: 20, lineHeight: 24 }}>{this.state.buttonText}</Text>
+        </TouchableOpacity>
+      </Container >
     );
   }
 }
