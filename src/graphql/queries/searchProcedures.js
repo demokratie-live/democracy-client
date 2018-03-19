7761584392c720216a28842c6f0c8eddf78695ea
Manuel Ruck <github@manuelruck.de>
2018-03-19 18:14:56 +0100

    fix search error

--- src/graphql/queries/searchProcedures.js
+++ src/graphql/queries/searchProcedures.js
@@ -10,7 +10,10 @@ export default gql`
       abstract
       voteDate
       submissionDate
-      activityIndex
+      activityIndex {
+        activityIndex
+        active
+      }
     }
   }
 `;
