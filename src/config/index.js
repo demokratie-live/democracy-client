97eefbfc108f5a525793b708ac788ac598acf9fe
Manuel Ruck <github@manuelruck.de>
2018-03-19 13:13:52 +0100

    remove ip from config

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,6 @@
 import Config from "react-native-config";
 
 export default {
-  GRAPHQL_URL:
-    process.env.GRAPHQL_URL ||
-    Config.GRAPHQL_URL ||
-    "http://192.168.99.100:3000/graphql",
-
+  GRAPHQL_URL: process.env.GRAPHQL_URL || Config.GRAPHQL_URL,
   PHONE_NUMBER: process.env.PHONE_NUMBER || Config.PHONE_NUMBER || ""
 };
