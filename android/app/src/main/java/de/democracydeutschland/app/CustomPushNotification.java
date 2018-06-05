3cc445c82c5671a31ff2e1f22e98c6db6f4d622e
Manuel Ruck <github@manuelruck.de>
2018-06-05 15:37:04 +0200

    fix android push notifications

--- android/app/src/main/java/de/democracydeutschland/app/CustomPushNotification.java
+++ android/app/src/main/java/de/democracydeutschland/app/CustomPushNotification.java
@@ -1,16 +1,47 @@
 package de.democracydeutschland.app;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import de.democracydeutschland.app.R;
 
 import com.wix.reactnativenotifications.core.AppLaunchHelper;
 import com.wix.reactnativenotifications.core.AppLifecycleFacade;
 import com.wix.reactnativenotifications.core.JsIOHelper;
 import com.wix.reactnativenotifications.core.notification.PushNotification;
 
+
+
 public class CustomPushNotification extends PushNotification {
 
     public CustomPushNotification(Context context, Bundle bundle, AppLifecycleFacade appLifecycleFacade, AppLaunchHelper appLaunchHelper, JsIOHelper jsIoHelper) {
         super(context, bundle, appLifecycleFacade, appLaunchHelper, jsIoHelper);
+    }
+
+    @Override
+    protected Notification.Builder getNotificationBuilder(PendingIntent intent) {
+        final Resources resources = mContext.getResources();
+
+        // First, get a builder initialized with defaults from the core class.
+        final Notification.Builder builder = super.getNotificationBuilder(intent);
+
+        // Set our custom overrides --
+
+        // Enable 'extended' layout (extends on down-stroke gesture):
+        final Notification.BigTextStyle extendedNotificationStyle =
+                new Notification.BigTextStyle()
+                        .bigText(mNotificationProps.getBody()); // "4:15 - 5:15 PM\nBig Conference Room"
+        builder.setStyle(extendedNotificationStyle);
+
+        // Set custom-action icon.
+        builder.setSmallIcon(R.drawable.ic_notification)
+                .setColor(resources.getColor(R.color.pushIcon)); // Blue-ish
+
+        return builder;
     }
 }
