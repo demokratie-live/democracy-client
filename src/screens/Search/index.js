84b34abc64f99d33d289ca1f9fdfbef4a5682637
Manuel Ruck <github@manuelruck.de>
2018-07-19 16:22:21 +0200

    fix add search history

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -286,7 +286,7 @@ export default withApollo(
           searchHistory: searchHistory
             ? searchHistory.map(({ term }) => term)
             : []
-        })
+        }),
       }),
 
       // Mutations
