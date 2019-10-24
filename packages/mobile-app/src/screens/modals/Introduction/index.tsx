f7f61149cc90fd5c37fb0eb8de00d46b5b8fb2f8
Manuel Ruck <github.com@manuelruck.de>
2019-10-24 13:22:55 +0700

    add safe area to instructions screen

--- packages/mobile-app/src/screens/modals/Introduction/index.tsx
+++ packages/mobile-app/src/screens/modals/Introduction/index.tsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/core';
 import { StackNavigationProp } from '@react-navigation/stack';
+import styled from 'styled-components/native';
+
 import { RootStackParamList } from '../../../routes';
 
 import { Slide } from '@democracy-deutschland/mobile-ui/src/components/Instruction';
 import { slidesData } from '@democracy-deutschland/mobile-ui/src/components/Instruction/data';
 import { Pager } from '@democracy-deutschland/mobile-ui/src/components/Pager';
 
+const SafeAreaView = styled.SafeAreaView`
+  flex: 1;
+`;
+
 const Introduction = () => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
   return (
-    <Pager>
-      {Object.keys(slidesData).map((screen, i) => (
-        <Slide
-          head={slidesData[screen].head}
-          images={slidesData[screen].images}
-          isNew={slidesData[screen].isNew}
-          nextSlide={
-            i + 1 === Object.keys(slidesData).length
-              ? () => {
-                  console.log('HIER');
-                  navigation.goBack();
-                }
-              : undefined
-          }
-        />
-      ))}
-    </Pager>
+    <SafeAreaView>
+      <Pager>
+        {Object.keys(slidesData).map((screen, i) => (
+          <Slide
+            head={slidesData[screen].head}
+            images={slidesData[screen].images}
+            isNew={slidesData[screen].isNew}
+            nextSlide={
+              i + 1 === Object.keys(slidesData).length
+                ? () => {
+                    console.log('HIER');
+                    navigation.goBack();
+                  }
+                : undefined
+            }
+          />
+        ))}
+      </Pager>
+    </SafeAreaView>
   );
 
   // return (
