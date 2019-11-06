56384c704dd9ac5ef5c41802d520cc7874cb127e
Manuel Ruck <github.com@manuelruck.de>
2019-11-06 13:29:11 +0700

    prepare testing with react native testing library

---
+++ packages/mobile-ui/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // The root of your source code, typically /src
+  // `<rootDir>` is a token Jest substitutes
+  roots: ['<rootDir>/src'],
+
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+
+  // Module file extensions for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  preset: 'react-native',
+};
