c26cc95d765ccd4dd3bfdccd6227959ebeebc4dc
Manuel Ruck <github.com@manuelruck.de>
2019-11-07 23:39:46 +0700

    add first running e2e test

--- packages/mobile-app/e2e/firstTest.spec.js
+++ packages/mobile-app/e2e/firstTest.spec.js
@@ -2,28 +2,32 @@
 
 describe('Example', () => {
   beforeEach(async () => {
-    console.log('HA 1');
-    console.log('HA 1.1', device, config);
     if (typeof device === 'undefined') {
       await detox.init(config);
     }
-    await device.reloadReactNative();
-    console.log('HA 2');
+    // await device.reloadReactNative();
   });
 
   it('should have welcome screen', async () => {
-    console.log('HA 3');
-    await expect(element(by.id('welcome'))).toBeVisible();
-    console.log('HA 4');
+    await element(by.text('Weiter')).tap();
+    await element(by.text('Weiter')).tap();
+    await element(by.text('Weiter')).tap();
+    await element(by.text('Weiter')).tap();
+    await expect(element(by.text('Vergleiche'))).toBeVisible();
   });
+  // it('should have instruction screen', async () => {
+  //   await expect(element(by.id('Instructions')))
+  //     .toBeVisible()
+  //     .withTimeout(10000);
+  // });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
+  // it('should show hello screen after tap', async () => {
+  //   await element(by.id('hello_button')).tap();
+  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
+  // });
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
+  // it('should show world screen after tap', async () => {
+  //   await element(by.id('world_button')).tap();
+  //   await expect(element(by.text('World!!!'))).toBeVisible();
+  // });
 });
