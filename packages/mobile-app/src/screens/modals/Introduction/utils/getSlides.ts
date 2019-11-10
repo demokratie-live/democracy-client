ba9c75c40649ea2464949fb02410038d03afd945
Manuel Ruck <github.com@manuelruck.de>
2019-11-10 13:21:24 +0700

    improve introduction screen show slide logic

---
+++ packages/mobile-app/src/screens/modals/Introduction/utils/getSlides.ts
@@ -0,0 +1,30 @@
+import {
+  Slide,
+  slidesData,
+} from '@democracy-deutschland/mobile-ui/src/components/Instruction';
+
+export const getSlides = ({
+  lastVersion,
+  registered,
+}: {
+  lastVersion: string | undefined;
+  registered: boolean;
+}): Slide[] => {
+  const slides: Slide[] = [];
+  switch (true) {
+    case !lastVersion || lastVersion < '1.2.2':
+      slides.push(
+        slidesData.Willkommen,
+        slidesData.Beobachte,
+        slidesData.Informiere,
+        slidesData.Stimme,
+        slidesData.Vergleiche,
+        slidesData.Analysiere,
+      );
+      break;
+  }
+  if (!registered) {
+    slides.push(slidesData.Registrieren);
+  }
+  return slides;
+};
