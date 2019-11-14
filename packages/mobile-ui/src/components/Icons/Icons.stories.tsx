34e293eeea43d0aa507556b3c9d620fcb7b8729e
Manuel Ruck <github.com@manuelruck.de>
2019-11-14 18:06:39 +0700

    add svgs

---
+++ packages/mobile-ui/src/components/Icons/Icons.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react-native';
+
+import { SvgDocument } from '../Icons';
+
+storiesOf('Icons', module).add('Document', () => <SvgDocument />);
