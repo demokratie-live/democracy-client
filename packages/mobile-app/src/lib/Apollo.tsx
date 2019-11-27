b738cbe75a0ac0632cca6f8e4f6aa12610ba9e00
Manuel Ruck <github.com@manuelruck.de>
2019-11-27 18:55:48 +0700

    fix android development emulator graphql uri

--- packages/mobile-app/src/lib/Apollo.tsx
+++ packages/mobile-app/src/lib/Apollo.tsx
@@ -3,10 +3,20 @@ import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { Platform } from 'react-native';
 
 const cache = new InMemoryCache();
+
+let graphQlUri = 'http://alpha.api.democracy-app.de';
+if (process.env.NODE_ENV === 'development') {
+  graphQlUri =
+    Platform.OS === 'android'
+      ? 'http://10.0.2.2:3000'
+      : 'http://localhost:3000';
+}
+
 const link = new HttpLink({
-  uri: 'http://127.0.0.1:3000',
+  uri: graphQlUri,
 });
 
 const client = new ApolloClient({
