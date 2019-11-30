0002a58ba23f261b3f504fc2ed4ce64b22359e64
Manuel Ruck <github.com@manuelruck.de>
2019-11-30 15:30:26 +0700

    push to same branch

--- .travis/push.sh
+++ .travis/push.sh
@@ -4,13 +4,11 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 commit_ios_files() {
-  git checkout -b gh-pages
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 upload_files() {
-  git remote add origin-pages https://${GITHUB_API_TOKEN}@github.com/demokratie-live democracy-app.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git push --quiet
 }
 setup_git
 commit_ios_files
