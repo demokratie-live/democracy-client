79f9b078679c48a493533dd620e3e0fc866d1c42
Manuel Ruck <github.com@manuelruck.de>
2019-11-30 14:52:16 +0700

    install nvm

--- .travis/ci_check_commands.sh
+++ .travis/ci_check_commands.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if ! [ -x "$(command -v nvm)" ]; then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+  nvm install node
+  nvm use node
+fi
+echo "Use nvm version: $(nvm --version)"
+
 if ! [ -x "$(command -v node)" ]; then
   echo 'Error: node is not installed.' >&2
   exit 1
