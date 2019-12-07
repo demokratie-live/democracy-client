35a54886705bb128277ded225eff7ff218855270
Manuel Ruck <github.com@manuelruck.de>
2019-12-07 11:40:53 +0700

    cleanup navigation

--- packages/mobile-app/src/screens/Sidebar/Sidebar.tsx
+++ packages/mobile-app/src/screens/Sidebar/Sidebar.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components/native';
 import { Background } from '@democracy-deutschland/mobile-ui/src/components/Sidebar/Background';
 import { Header } from '@democracy-deutschland/mobile-ui/src/components/Sidebar/Header';
 import DrawerItemList from './DrawerItemList';
 import { RootStackParamList } from '../../routes';
-import { RouteProp, useRoute } from '@react-navigation/core';
+import { RouteProp } from '@react-navigation/core';
+import { InitialStateContext } from '../../context/InitialStates';
 
 const SafeAreaView = styled.SafeAreaView`
   flex: 1;
@@ -18,15 +19,14 @@ type SidebarScreenRouteProp = RouteProp<RootStackParamList, 'Sidebar'>;
 declare type Props = React.ComponentProps<typeof DrawerItemList>;
 
 export const Sidebar: React.FC<Props> = props => {
-  const route = useRoute<SidebarScreenRouteProp>();
-  const { registered } = route.params;
+  const { isVerified } = useContext(InitialStateContext);
   return (
     <Container>
       <Background />
       <SafeAreaView>
         <Header
           onPress={() => {}}
-          label={registered ? 'verifizierter Nutzer' : 'unverifizierter Nutzer'}
+          label={isVerified ? 'verifizierter Nutzer' : 'unverifizierter Nutzer'}
         />
         <DrawerItemList {...props} />
       </SafeAreaView>
