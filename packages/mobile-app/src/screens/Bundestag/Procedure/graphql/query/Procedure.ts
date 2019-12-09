3d41a414fa0d9c403fcf63b23f673723f217a1b6
Manuel Ruck <github.com@manuelruck.de>
2019-12-09 23:52:49 +0700

    start detail view

---
+++ packages/mobile-app/src/screens/Bundestag/Procedure/graphql/query/Procedure.ts
@@ -0,0 +1,46 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  query Procedure($id: ID!) {
+    procedure(id: $id) {
+      _id
+      procedureId
+      title
+      tags
+      abstract
+      voteDate
+      voteEnd
+      notify
+      list
+      type
+      subjectGroups
+      submissionDate
+      currentStatus
+      currentStatusHistory
+      importantDocuments {
+        editor
+        type
+        url
+        number
+      }
+      voteResults {
+        yes
+        abstination
+        no
+        notVoted
+        decisionText
+        namedVote
+        partyVotes {
+          main
+          party
+          deviants {
+            yes
+            abstination
+            no
+            notVoted
+          }
+        }
+      }
+    }
+  }
+`;
