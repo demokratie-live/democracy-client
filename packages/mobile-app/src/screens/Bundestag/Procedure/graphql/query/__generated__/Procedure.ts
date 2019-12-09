8f035cfd9675c9b1bf00465c0db7a35c2775349b
Manuel Ruck <github.com@manuelruck.de>
2019-12-09 23:52:49 +0700

    start detail view

---
+++ packages/mobile-app/src/screens/Bundestag/Procedure/graphql/query/__generated__/Procedure.ts
@@ -0,0 +1,71 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { ListType, VoteSelection } from "./../../../../../../../__generated__/globalTypes";
+
+// ====================================================
+// GraphQL query operation: Procedure
+// ====================================================
+
+export interface Procedure_procedure_importantDocuments {
+  __typename: "Document";
+  editor: string | null;
+  type: string | null;
+  url: string | null;
+  number: string | null;
+}
+
+export interface Procedure_procedure_voteResults_partyVotes_deviants {
+  __typename: "Deviants";
+  yes: number | null;
+  abstination: number | null;
+  no: number | null;
+  notVoted: number | null;
+}
+
+export interface Procedure_procedure_voteResults_partyVotes {
+  __typename: "PartyVote";
+  main: VoteSelection | null;
+  party: string;
+  deviants: Procedure_procedure_voteResults_partyVotes_deviants | null;
+}
+
+export interface Procedure_procedure_voteResults {
+  __typename: "VoteResult";
+  yes: number | null;
+  abstination: number | null;
+  no: number | null;
+  notVoted: number | null;
+  decisionText: string | null;
+  namedVote: boolean | null;
+  partyVotes: (Procedure_procedure_voteResults_partyVotes | null)[] | null;
+}
+
+export interface Procedure_procedure {
+  __typename: "Procedure";
+  _id: string;
+  procedureId: string;
+  title: string;
+  tags: (string | null)[] | null;
+  abstract: string | null;
+  voteDate: any | null;
+  voteEnd: any | null;
+  notify: boolean | null;
+  list: ListType | null;
+  type: string | null;
+  subjectGroups: string[] | null;
+  submissionDate: any | null;
+  currentStatus: string | null;
+  currentStatusHistory: (string | null)[] | null;
+  importantDocuments: (Procedure_procedure_importantDocuments | null)[] | null;
+  voteResults: Procedure_procedure_voteResults | null;
+}
+
+export interface Procedure {
+  procedure: Procedure_procedure;
+}
+
+export interface ProcedureVariables {
+  id: string;
+}
