3dee5852f16578d7e092521a7173cd68d78ea944
Manuel Ruck <github.com@manuelruck.de>
2019-12-17 22:05:30 +0700

    save own voting local

---
+++ packages/mobile-app/src/context/LocalVotes/index.tsx
@@ -0,0 +1,73 @@
+import React, { createContext, useState, FC, useEffect } from 'react';
+import VotesLocal, { ChainEntry, SetLocalVote } from '../../lib/VotesLocal';
+
+interface LocalVotesInterface {
+  localVotes: ChainEntry[];
+  setLocalVote: ({
+    procedureId,
+    selection,
+    constituency,
+  }: SetLocalVote) => void;
+  getLocalVoteSelection: (
+    procedureId: string,
+  ) => ChainEntry['selection'] | undefined;
+}
+
+const defaults: LocalVotesInterface = {
+  localVotes: [],
+  setLocalVote: () => {
+    throw new Error('LocalVotesContext: setLocalVote function is not defined');
+  },
+  getLocalVoteSelection: () => {
+    throw new Error(
+      'LocalVotesContext: getLocalVoteSelection function is not defined',
+    );
+  },
+};
+
+export const LocalVotesContext = createContext<LocalVotesInterface>(defaults);
+
+export const LocalVotesProvider: FC = ({ children }) => {
+  const [localVotes, setLocalVotes] = useState<ChainEntry[]>([]);
+
+  useEffect(() => {
+    VotesLocal.getVotes().then(votes => {
+      setLocalVotes(votes);
+    });
+  }, []);
+
+  const setLocalVote = ({
+    procedureId,
+    selection,
+    constituency,
+  }: Pick<ChainEntry, 'procedureId' | 'selection' | 'constituency'>) => {
+    VotesLocal.setVote({ procedureId, selection, constituency }).then(
+      success => {
+        if (success) {
+          VotesLocal.getVotes().then(votes => {
+            setLocalVotes(votes);
+          });
+        }
+      },
+    );
+  };
+
+  const getLocalVoteSelection = (procedureId: string) => {
+    const localVote = localVotes.find(p => p.procedureId === procedureId);
+    if (localVote) {
+      return localVote.selection;
+    }
+    return;
+  };
+
+  return (
+    <LocalVotesContext.Provider
+      value={{
+        localVotes,
+        setLocalVote,
+        getLocalVoteSelection,
+      }}>
+      {children}
+    </LocalVotesContext.Provider>
+  );
+};
