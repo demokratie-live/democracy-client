121fe1907bb880dee4b53ce0f2ff247499195bc8
Manuel Ruck <github.com@manuelruck.de>
2019-12-18 17:48:52 +0700

    add push notification library

--- packages/mobile-app/android/app/src/main/java/de/democracydeutschland/app/refactor/MainApplication.java
+++ packages/mobile-app/android/app/src/main/java/de/democracydeutschland/app/refactor/MainApplication.java
@@ -10,6 +10,9 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+// react-native-notifications
+import com.wix.reactnativenotifications.RNNotificationsPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -25,6 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+
+          // react-native-notifications
+          packages.add(new RNNotificationsPackage(MainApplication.this));
+          
           return packages;
         }
 
