bd3a5caf9e6ec760075ae40d4d8f7a4a8a4f0bf6
Manuel Ruck <github.com@manuelruck.de>
2019-12-19 11:49:51 +0700

    Prepare searching

---
+++ packages/mobile-app/src/screens/Bundestag/Search/graphql/query/__generated__/MostSearched.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: MostSearched
+// ====================================================
+
+export interface MostSearched_mostSearched {
+  __typename: "SearchTerm";
+  term: string;
+}
+
+export interface MostSearched {
+  mostSearched: MostSearched_mostSearched[];
+}
