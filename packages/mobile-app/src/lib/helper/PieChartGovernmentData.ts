7b895fb224f6a5aa373a67a71fb971ab24fafd54
Manuel Ruck <github.com@manuelruck.de>
2019-12-19 20:46:31 +0700

    fix search

---
+++ packages/mobile-app/src/lib/helper/PieChartGovernmentData.ts
@@ -0,0 +1,38 @@
+import { ProceduresList_procedures_voteResults } from '../../screens/Bundestag/List/graphql/query/__generated__/ProceduresList';
+import { Slice } from '@democracy-deutschland/mobile-ui/src/components/shared/Charts/PieChart';
+
+interface Props {
+  votedGovernment?: boolean | null;
+  voteResults?: ProceduresList_procedures_voteResults | null;
+}
+
+export const pieChartGovernmentData = ({
+  votedGovernment,
+  voteResults,
+}: Props): Slice[] | undefined => {
+  if (votedGovernment && voteResults) {
+    // TODO improve graphql types for this
+    const sumVotes =
+      (voteResults.yes || 0) +
+      (voteResults.abstination || 0) +
+      (voteResults.no || 0);
+    return [
+      {
+        color: '#99C93E',
+        percent: (voteResults.yes || 0) / sumVotes,
+        large: voteResults.governmentDecision === 'YES',
+      },
+      {
+        color: '#4CB0D8',
+        percent: (voteResults.abstination || 0) / sumVotes,
+        large: voteResults.governmentDecision === 'ABSTINATION',
+      },
+      {
+        color: '#D43194',
+        percent: (voteResults.no || 0) / sumVotes,
+        large: voteResults.governmentDecision === 'NO',
+      },
+    ];
+  }
+  return;
+};
