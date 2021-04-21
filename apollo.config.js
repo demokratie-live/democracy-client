2a3d0a4224f71298b28e68e034559570c9c72fff
Manuel Ruck <github.com@manuelruck.de>
2021-04-21 19:08:21 +0200

    ignore graphql rest api warning
    Signed-off-by: Manuel Ruck <github.com@manuelruck.de>

--- apollo.config.js
+++ apollo.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   client: {
     includes: ["./packages/mobile-app/src/**/*.ts"],
+    excludes: [
+      "./packages/mobile-app/src/screens/Sidebar/Donate/graphql/query/donationStatus.ts",
+    ],
     service: {
       name: "DEMOCRACY API Local",
       url: "https://internal.api.democracy-app.de",
-      skipSSLValidation: true
-    }
-  }
+      skipSSLValidation: true,
+    },
+  },
 };
