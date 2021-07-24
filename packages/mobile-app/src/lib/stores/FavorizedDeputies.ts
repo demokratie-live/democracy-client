7b68040fbb921c9f5b853503dae9cc000ac67788
Manuel Ruck <github.com@manuelruck.de>
2021-07-24 16:07:30 +0200

    add default favourized deputies
    Signed-off-by: Manuel Ruck <github.com@manuelruck.de>

--- packages/mobile-app/src/lib/stores/FavorizedDeputies.ts
+++ packages/mobile-app/src/lib/stores/FavorizedDeputies.ts
@@ -6,19 +6,75 @@
  * */
 
 import AsyncStorage from '@react-native-community/async-storage';
+import { STORAGE_KEY_CONSTITUENCY } from '../../context/Constituency';
+import { DEPUTY_SEARCH } from '../../screens/Abgeordnete/graphql/query/deputies';
+import {
+  Deputies,
+  DeputiesVariables,
+} from '../../screens/Abgeordnete/graphql/query/__generated__/Deputies';
+import { client } from '../Apollo';
 
 export type FavorizedDeputiesStore = string[];
 
 const STORAGEE_KEY = 'FAVORIZED_DEPUTIES';
 
-export const getFavorizedDeputies = async () => {
-  const data = await AsyncStorage.getItem(STORAGEE_KEY);
-  return (data ? JSON.parse(data) : []) as FavorizedDeputiesStore;
-};
-
 const saveFavorizedDeputies = (ids: FavorizedDeputiesStore): Promise<void> => {
   const data = JSON.stringify(ids);
   return AsyncStorage.setItem(STORAGEE_KEY, data);
+};
+
+const getInitialFavorizedDeputies = async () => {
+  let initialDeputies = [
+    '519324',
+    '523750',
+    '518092',
+    '521640',
+    '524466',
+    '518176',
+  ];
+  const constituency = await AsyncStorage.getItem(STORAGE_KEY_CONSTITUENCY);
+  if (constituency) {
+    const constituencyDeputies = await client
+      .query<Deputies, DeputiesVariables>({
+        query: DEPUTY_SEARCH,
+        variables: {
+          limit: 5,
+          offset: 0,
+          filterTerm: constituency,
+        },
+      })
+      .then(({ data }) => {
+        console.log('fav', data, {
+          limit: 5,
+          offset: 0,
+          filterTerm: constituency,
+        });
+        return data.deputies.data.length > 0
+          ? data.deputies.data.map(deputy => deputy.webId)
+          : [];
+      });
+    initialDeputies = [...constituencyDeputies, ...initialDeputies].filter(
+      (v, i, a) => a.indexOf(v) === i,
+    );
+  }
+  return initialDeputies;
+};
+
+export const getFavorizedDeputies = async () => {
+  const favorizedDeputies = await AsyncStorage.getItem(
+    STORAGEE_KEY,
+  ).then(data => (data ? (JSON.parse(data) as string[]) : undefined));
+
+  let initialDeputies: string[] = [];
+
+  if (!favorizedDeputies) {
+    initialDeputies = await getInitialFavorizedDeputies();
+    saveFavorizedDeputies(initialDeputies);
+  }
+
+  return (favorizedDeputies
+    ? favorizedDeputies
+    : initialDeputies) as FavorizedDeputiesStore;
 };
 
 export const addFavorizedDeputy = async (id: string) => {
