2baa6bc8a091703e1426cfd72f92741032b56e31
Manuel Ruck <github.com@manuelruck.de>
2021-10-30 15:54:15 +0200

    fix #1539
    Signed-off-by: Manuel Ruck <github.com@manuelruck.de>

--- packages/mobile-app/src/screens/Sidebar/Parlaments/Parlaments.styled.ts
+++ packages/mobile-app/src/screens/Sidebar/Parlaments/Parlaments.styled.ts
@@ -1,14 +1,13 @@
 import { styled } from '../../../styles';
 
-export const Container = styled.View`
-  margin-horizontal: ${({ theme }) => theme.spaces.default};
-`;
+export const Container = styled.View``;
 
 export const List = styled.View`
   margin-bottom: ${({ theme }) => theme.spaces.default};
 `;
 
 export const Headline = styled.Text`
+  margin-horizontal: ${({ theme }) => theme.spaces.default};
   color: ${({ theme }) => theme.colors.text.secondary};
   text-transform: uppercase;
 `;
