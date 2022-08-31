6c857c8eb9364c0cd59b24281bca0cf809d0a24a
Manuel Ruck <git@manuelruck.de>
2022-08-31 20:59:27 +0200

    Add delete local votes action to Dev screen
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- src/routes/Sidebar/DevScreen.tsx
+++ src/routes/Sidebar/DevScreen.tsx
@@ -1,20 +1,30 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import React from 'react';
-import { Button } from 'react-native';
+import { Alert, Button } from 'react-native';
+import VotesLocal from '../../lib/VotesLocal';
 
 export const DevScreen: React.FC = () => {
   return (
-    <Button
-      title="Delete auth"
-      onPress={() => {
-        AsyncStorage.removeItem('auth_refreshToken');
-        AsyncStorage.removeItem('auth_token');
-        AsyncStorage.removeItem('auth_phoneHash');
-        AsyncStorage.removeItem('verification_code_resend_time');
-        AsyncStorage.removeItem('verification_code_expire_time');
-        AsyncStorage.removeItem('verification_tmp_phone_number');
-        console.log('auth deleted');
-      }}
-    />
+    <>
+      <Button
+        title="Delete auth"
+        onPress={() => {
+          AsyncStorage.removeItem('auth_refreshToken');
+          AsyncStorage.removeItem('auth_token');
+          AsyncStorage.removeItem('auth_phoneHash');
+          AsyncStorage.removeItem('verification_code_resend_time');
+          AsyncStorage.removeItem('verification_code_expire_time');
+          AsyncStorage.removeItem('verification_tmp_phone_number');
+          Alert.alert('auth deleted');
+        }}
+      />
+      <Button
+        title="Delete local votes"
+        onPress={() => {
+          VotesLocal.reset();
+          Alert.alert('local votes deleted');
+        }}
+      />
+    </>
   );
 };
