754e2906cb0f2bf57acc332dca4dac82b8ff3211
Manuel Ruck <git@manuelruck.de>
2023-05-21 19:23:22 +0200

    add description to sync votes screen
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- src/screens/SyncVotes/index.tsx
+++ src/screens/SyncVotes/index.tsx
@@ -32,6 +32,8 @@ const VoteTextArea = styled.TextInput`
   color: ${({ theme }) => theme.colors.text.primary};
 `;
 
+const Text = styled.Text``;
+
 export const SyncVotesScreen = () => {
   const [showTextField, setShowTextField] = useState<boolean>(false);
   const [text, setText] = useState('');
@@ -58,32 +60,61 @@ export const SyncVotesScreen = () => {
 
   return (
     <Container>
+      <Text>So überträgst Du Deine lokalen Stimmen auf ein andres Gerät:</Text>
+      <Text>------- AUF DEINEM BISHERIGEN GERÄT -------</Text>
+      <Text>
+        {
+          '1. Tippe auf den Button "STIMMEN KOPIEREN" und exportiere Deine lokalen Stimmen in Textform'
+        }
+      </Text>
+      <Text>
+        2. Wähle eine der angebotenen Möglichkeiten, z.B. durch Mail an Dich selbst, Deine
+        Abstimmungsdaten zu sichern
+      </Text>
       {!showTextField ? (
         <Button variant="primary" onPress={shareVotes}>
-          Abstimmungsdaten Abrufen
+          Stimmen kopieren
         </Button>
       ) : null}
 
-      <Button variant="primary" onPress={() => setShowTextField(v => !v)}>
-        {!showTextField ? 'Abstimmungsdaten Einfügen' : 'Abbrechen'}
-      </Button>
-      {showTextField ? (
-        <VoteTextArea
-          style={{ borderColor: 'black', borderWidth: 1, width: '100%', height: 200 }}
-          multiline
-          onChangeText={setText}
-          placeholder='{"data": "..."}'
-          placeholderTextColor="grey"
-        >
-          {text}
-        </VoteTextArea>
-      ) : null}
+      <Text>------- AUF DEINEM ANDEREN GERÄT -------</Text>
+      <Text>
+        1. Kopiere den auf Deinem alten Gerät gesicherten Text in die Zwischenablage Deines neuen
+        Geräts
+      </Text>
+      <Text>2. Öffne DEMOCRACY auf Deinem neuen Gerät</Text>
+      <Text>
+        {
+          '3. Tippe auf Menu > Einstellungen > Stimmen übertragen und wähle "ABSTIMMUNGSDATEN EINFÜGEN"'
+        }
+      </Text>
+      <Text>
+        {'4. Füge den gesamten Text {"283063":… in das Dialogfeld ein und wähle "SPEICHERN"'}
+      </Text>
 
       {showTextField ? (
-        <Button variant="primary" onPress={onSave}>
-          Speichern
+        <>
+          <VoteTextArea
+            style={{ borderColor: 'black', borderWidth: 1, width: '100%', height: 200 }}
+            multiline
+            onChangeText={setText}
+            placeholder='{"data": "..."}'
+            placeholderTextColor="grey"
+          >
+            {text}
+          </VoteTextArea>
+          <Button variant="primary" onPress={onSave}>
+            Speichern
+          </Button>
+          <Button variant="danger-secondary" onPress={() => setShowTextField(v => !v)}>
+            Abbrechen
+          </Button>
+        </>
+      ) : (
+        <Button variant="primary" onPress={() => setShowTextField(v => !v)}>
+          Stimmen Einfügen
         </Button>
-      ) : null}
+      )}
     </Container>
   );
 };
