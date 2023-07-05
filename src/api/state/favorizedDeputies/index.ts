14742c771beb9cc9917ab6cf322757ab633192cd
Manuel Ruck <git@manuelruck.de>
2023-07-05 15:14:19 +0200

    fix favorized deputies local storage key
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- src/api/state/favorizedDeputies/index.ts
+++ src/api/state/favorizedDeputies/index.ts
@@ -3,21 +3,31 @@ import { atom, atomFamily } from 'recoil';
 import { localStorageEffect } from '../effects/localStorageEffect';
 import { ParlamentIdentifier } from '../parlament';
 
-const STORAGE_KEY = 'FAVORIZED_DEPUTIES_1_5_5';
+const STORAGE_KEY = 'FAVORIZED_DEPUTIES';
 const STORAGE_KEY_DEPRECATED = 'FAVORIZED_DEPUTIES';
 
 export const favorizedDeputiesState = atomFamily<string[], ParlamentIdentifier>({
   key: 'favorizedDeputiesState',
   default: async param => {
-    const deprecatedFavorized = await AsyncStorage.getItem(STORAGE_KEY_DEPRECATED).then(value => {
-      if (value !== null) {
-        return JSON.parse(value) as string[];
-      }
-    });
+    const favorizedDeputies = await AsyncStorage.getItem(STORAGE_KEY).then(data =>
+      data ? (JSON.parse(data) as string[]) : undefined,
+    );
+    const deprecatedFavorized = await AsyncStorage.getItem(STORAGE_KEY_DEPRECATED)
+      .then(value => {
+        if (value !== null) {
+          return JSON.parse(value) as string[];
+        }
+      })
+      .finally(() => {
+        AsyncStorage.removeItem(STORAGE_KEY_DEPRECATED);
+      });
     if (param === 'BT-19') {
-      return deprecatedFavorized ?? ['519324', '523750', '518092', '521640', '524466', '518176'];
+      return (
+        favorizedDeputies ??
+        deprecatedFavorized ?? ['519324', '523750', '518092', '521640', '524466', '518176']
+      );
     } else if (param === 'BT-20') {
-      return deprecatedFavorized ?? [];
+      return favorizedDeputies ?? deprecatedFavorized ?? [];
     }
     return [];
   },
